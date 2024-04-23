684f53256de415f42619d3c8e3b96631157dad40
naveenbabu04 <saiajithnaveenbabu.bahunadham1@ingka.com>
2024-04-23 17:05:26 +0530

    some changes

--- booking/index.ts
+++ booking/index.ts
@@ -1,46 +1,19 @@
-import express from "express";
-import { json } from "body-parser";
-import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-import { NotFoundError } from "./src/errors/not-found-error";
-import { jsonErrorMiddle } from "./src/api/middlewares/json-error-middleware";
-import { BookingRouter } from "./src/api/routing";
-import { openApiMiddleware } from "./src/api/middlewares/open-api-middleware";
-import swaggerUi from "swagger-ui-express";
-import YAML from 'yamljs';
-import path from 'path'
-
-const app = express();
-const mongoURL=process.env.MONGO_URL || "mongodb://localhost:27017/travel-agency-booking"
-app.set('trust proxy',true);
-app.use(cookieSession({
-    signed:false,
-    secure:true,
-    httpOnly:true
-}))
-app.use(json())
-const oasFile = YAML.load(path.join(__dirname , './src/api/oas.yaml'));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(oasFile));
-app.use(openApiMiddleware());
-app.all('*',async(req,res,next) =>{
-    try {
-      throw new NotFoundError()   
-    } catch (error) {
-        next(error)
-    }
-});
-
-app.use(jsonErrorMiddle);
+import mongoose from 'mongoose';
+import { app } from './app';
+import http from 'http'
+const mongoURL=process.env.MONGOURL || "mongodb://localhost:27017/travel-agency-vehicles"
 const start=async()=>{
     try {
         mongoose.connect(mongoURL)
-        app.listen(4002,()=>
+        console.log("🍀 Successfly connected to MongoDB LocalService");
+        
+        http.createServer(app).listen(4002,()=>
         {
-            console.log("Running on port ",4002);
+            console.log(`✅ Service is running on http://localhost:4002/api/booking/docs`);
             
         }) 
     } catch (error) {
-        
+        console.log(`❌ Error in Connection ${error}`);
     }
     
 }
